a8aaa975e16e22e0dfd3be33d013e2f2aa877943
Pjotr Savitski <pjotr.savitski@gmail.com>
2020-08-28 00:31:14 +0300

    Added tansition to master expert view

--- resources/js/components/MasterExpertView.vue
+++ resources/js/components/MasterExpertView.vue
@@ -20,59 +20,61 @@
             </b-dropdown>
         </div>
 
-        <b-tabs
-            v-model="tabIndex"
-            content-class="mt-4"
-            active-nav-item-class="bg-primary text-white"
-            fill
-            v-if="!selectedLanguage"
-        >
-            <b-tab lazy>
-                <template v-slot:title>
-                    Questions
-                    <b-badge :variant="tabTitleBadgeVariant(0)" pill>{{ questions.length }}</b-badge>
+        <transition name="fade" mode="out-in">
+            <b-tabs
+                v-model="tabIndex"
+                content-class="mt-4"
+                active-nav-item-class="bg-primary text-white"
+                fill
+                v-if="!selectedLanguage"
+            >
+                <b-tab lazy>
+                    <template v-slot:title>
+                        Questions
+                        <b-badge :variant="tabTitleBadgeVariant(0)" pill>{{ questions.length }}</b-badge>
+                    </template>
+
+                    <questions-table :items="questions"></questions-table>
+                </b-tab>
+                <b-tab lazy>
+                    <template v-slot:title>
+                        Pending questions
+                        <b-badge :variant="tabTitleBadgeVariant(1)" pill>{{ pendingQuestions.length }}</b-badge>
+                    </template>
+
+                    <pending-questions-table :items="pendingQuestions"></pending-questions-table>
+                </b-tab>
+                <b-tab lazy>
+                    <template v-slot:title>
+                        Answers
+                        <b-badge :variant="tabTitleBadgeVariant(2)" pill>{{ answers.length }}</b-badge>
+                    </template>
+
+                    <answers-table :items="answers"></answers-table>
+                </b-tab>
+            </b-tabs>
+
+            <b-card
+                v-if="selectedLanguage && showLanguageExpertView"
+            >
+                <template v-slot:header>
+                    <h3 class="mb-0">
+                        Country SELFIE Expert {{ selectedLanguage.name }}
+                        <b-button
+                            v-b-tooltip
+                            title="Clear selected language"
+                            @click="onLanguageSelected(null)"
+                            size="sm"
+                            variant="outline-secondary"
+                        >
+                            <font-awesome-icon :icon="['fas', 'times']" />
+                        </b-button>
+                    </h3>
                 </template>
 
-                <questions-table :items="questions"></questions-table>
-            </b-tab>
-            <b-tab lazy>
-                <template v-slot:title>
-                    Pending questions
-                    <b-badge :variant="tabTitleBadgeVariant(1)" pill>{{ pendingQuestions.length }}</b-badge>
-                </template>
-
-                <pending-questions-table :items="pendingQuestions"></pending-questions-table>
-            </b-tab>
-            <b-tab lazy>
-                <template v-slot:title>
-                    Answers
-                    <b-badge :variant="tabTitleBadgeVariant(2)" pill>{{ answers.length }}</b-badge>
-                </template>
-
-                <answers-table :items="answers"></answers-table>
-            </b-tab>
-        </b-tabs>
-
-        <b-card
-            v-if="selectedLanguage"
-        >
-            <template v-slot:header>
-                <h3 class="mb-0">
-                    Country SELFIE Expert {{ selectedLanguage.name }}
-                    <b-button
-                        v-b-tooltip
-                        title="Clear selected language"
-                        @click="onLanguageSelected(null)"
-                        size="sm"
-                        variant="outline-secondary"
-                    >
-                        <font-awesome-icon :icon="['fas', 'times']" />
-                    </b-button>
-                </h3>
-            </template>
-
-            <language-expert-view :language="selectedLanguage.code"></language-expert-view>
-        </b-card>
+                <language-expert-view :language="selectedLanguage.code"></language-expert-view>
+            </b-card>
+        </transition>
     </div>
 </template>
 
@@ -95,7 +97,8 @@
         data() {
             return {
                 tabIndex: 0,
-                selectedLanguage: null
+                selectedLanguage: null,
+                showLanguageExpertView: true
             }
         },
         computed: {
@@ -117,7 +120,15 @@
                 return this.tabIndex === index ? 'light' : 'secondary'
             },
             onLanguageSelected(language) {
-                this.selectedLanguage = language
+                if (this.selectedLanguage && language && this.selectedLanguage.code !== language.code) {
+                    this.showLanguageExpertView = false
+                    setTimeout(() => {
+                        this.selectedLanguage = language
+                        this.showLanguageExpertView = true
+                    }, 250)
+                } else {
+                    this.selectedLanguage = language
+                }
             }
         },
         created() {
@@ -127,3 +138,12 @@
         }
     }
 </script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
