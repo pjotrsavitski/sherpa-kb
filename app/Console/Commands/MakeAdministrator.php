2be8f7107efa6dd96dfc7c7e4f1b46f6bc5ae94f
Pjotr Savitski <pjotr.savitski@gmail.com>
2020-09-18 17:10:32 +0300

    Added command to assing an administrator role

---
+++ app/Console/Commands/MakeAdministrator.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\User;
+
+class MakeAdministrator extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'permission:make-administrator {user}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Assign an administrator role to a user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $role = 'administrator';
+        $id = $this->argument('user');
+        $user = User::find($id);
+
+        if (!$user) {
+            $this->error("Could not find a user with identifier of {$id}!");
+            return 1;
+        }
+
+        if (!$user->hasRole($role)) {
+            $user->assignRole($role);
+            $this->info("Administrator role was assigned a user {$user->name}");
+            return 0;
+        }
+        
+        $this->line("User {$user->name} already has an administrator role!");
+
+        return 0;
+    }
+}
