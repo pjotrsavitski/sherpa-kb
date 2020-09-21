842ba86828775a3bfe46fd563f438a54a92f7440
Pjotr Savitski <pjotr.savitski@gmail.com>
2020-09-21 16:40:29 +0300

    Using flex layout instead of float to the right for search form

--- resources/js/components/TableSearchDescriptions.vue
+++ resources/js/components/TableSearchDescriptions.vue
@@ -1,29 +1,31 @@
 <template>
-   <b-form
-        inline
-        @submit="onSearchSubmit"
-        @reset="onSearchReset"
-        class="float-right mb-2"
-    >
-        <b-form-group
-            id="input-group-search-text"
+    <div class="d-flex flex-row-reverse">
+        <b-form
+            inline
+            @submit="onSearchSubmit"
+            @reset="onSearchReset"
+            class="mb-2"
         >
-            <b-input-group>
-                <b-form-input
-                    id="search-text"
-                    v-model="$parent.form.search"
-                    type="text"
-                    placeholder="Search text"
-                    trim
-                    debounce="500"
-                >
-                </b-form-input>
-                <b-input-group-append>
-                    <b-button type="reset" variant="outline-danger">Reset</b-button>
-                </b-input-group-append>
-            </b-input-group>
-        </b-form-group>
-    </b-form>
+            <b-form-group
+                id="input-group-search-text"
+            >
+                <b-input-group>
+                    <b-form-input
+                        id="search-text"
+                        v-model="$parent.form.search"
+                        type="text"
+                        placeholder="Search text"
+                        trim
+                        debounce="500"
+                    >
+                    </b-form-input>
+                    <b-input-group-append>
+                        <b-button type="reset" variant="outline-danger">Reset</b-button>
+                    </b-input-group-append>
+                </b-input-group>
+            </b-form-group>
+        </b-form>
+    </div>
 </template>
 
 <script>
